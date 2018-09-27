da6cfa4a10bbc34fd48b960c61814de23ece935a
AnhHoang <CodingSolution97@gmail.com>
2018-09-27 23:50:59 +0700

    finished

--- StoreLink/script.js
+++ StoreLink/script.js
@@ -1,30 +1,75 @@
-let title = document.getElementById("title");
+function saveBookmark() {
+    let siteName = document.getElementById('siteName').value;
+    let siteUrl = document.getElementById('siteUrl').value;
 
-let url = document.getElementById("url");
-
-let form = document.getElementById("form");
-
-let result = document.getElementById("result");
-
-let adding = function(){
-    if(!checkTitleEmpty() && !checkUrlEmpty()){
-        let div = document.createElement("div");
-        let content = document.createTextNode({
-            title: title.value,
-            url : url.value,
-        });
-        div.appendChild(content);
-        result.appendChild(div);
+    if (!validateForm(siteName, siteUrl)) {
+        return false;
     }
     
-};
+    let bookmark = {
+        name: siteName,
+        url: siteUrl,
+    };
 
-let checkTitleEmpty = function(){
-    return (title.value === "") ? true : false;
+    if(localStorage.getItem('bookmarks') === null){
+        let bookmarks = [];
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+        let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+    //reset form
+    document.getElementById("form").reset();
+    //reset the page to load data
+    fetchData();
 }
 
-var checkUrlEmpty = function() {
-    return (url.value === "") ? true : false;
+function fetchData() {
+    let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    console.log(bookmarks);
+    let result = document.getElementById('result');
+    result.innerHTML = '';
+    result.innerHTML += bookmarks.map(function(site){
+        return `<div class="item" style="font-size: 24px">${site.name}
+                    <div>
+                        <a href="${site.url}" target="_blank" class="link">Visit</a>
+                    </div>
+                    <div>
+                        <input type="button" value="delete" class="delete" onclick="deleteBookmark(\'${site.url}\')">
+                    </div>
+                </div>`;
+    }).join('');
 }
 
-console.log(result);
+function deleteBookmark(url){
+    let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    for(let i = 0; i < bookmarks.length; i++){
+        if(bookmarks[i].url === url){
+            bookmarks.splice(i, 1);
+            console.log('delete successfully');
+        }
+    }
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchData();
+}
+
+
+// Validate Form
+function validateForm(siteName, siteUrl) {
+    if (!siteName || !siteUrl) {
+        alert('Please fill in the form');
+        return false;
+    }
+
+    let expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    let regex = new RegExp(expression);
+
+    if (!siteUrl.match(regex)) {
+        alert('Please use a valid URL');
+        return false;
+    }
+
+    return true;
+}
