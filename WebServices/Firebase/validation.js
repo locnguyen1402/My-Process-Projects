b9739d307d874a878776ca86352ebbadc4d1898e
AnhNguyen <CodingSolution97@gmail.com>
2018-12-05 16:04:36 +0700

    a

--- WebServices/Firebase/validation.js
+++
@@ -1,9 +0,0 @@
-export class validation{
-    constructor(){
-        console.log('create instance of validation class');
-    }
-
-    checkEmpty(name, phone, description){
-        return (name.value == '' || phone.value == '' || description.value == '');
-    }
-}
